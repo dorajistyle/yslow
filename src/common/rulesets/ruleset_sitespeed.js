a3cd9af07f9d14d70c601e6cd655b6a9b426b6b8
Peter Hedenskog <peter@soulgalore.com>
2012-08-24 23:48:09 +0200

    2 new rules

--- src/common/rulesets/ruleset_sitespeed.js
+++ src/common/rulesets/ruleset_sitespeed.js
@@ -32,16 +32,16 @@ YSLOW.registerRule({
   name: 'Load 3rd party JS asyncrhonously',
   info: "Use the JavaScript snippets that load the JS files asyncrhonously " +
         "in order to speed up the user experience.",
-  category: ['Common'],
+  category: ['js'],
   config: {},
   url: 'http://www.phpied.com/3PO#async',
 
   lint: function (doc, cset, config) {
-    var scripts = doc.getElementsByTagName('script');
-    var comps = cset.getComponentsByType('js');
-    var comp;
-    var offenders = {};
-    var score = 100;
+    var scripts = doc.getElementsByTagName('script'), 
+    comps = cset.getComponentsByType('js'),
+    comp, offenders = {}, 
+    offender_comps = [], 
+    score = 100;
     
     // find offenders
     for (i = 0, len = scripts.length; i < len; i++) {
@@ -54,7 +54,6 @@ YSLOW.registerRule({
     }
 
     // match offenders to YSLOW components
-    var offender_comps = [];
     for (var i = 0; i < comps.length; i++) {
       if (offenders[comps[i].url]) {
         offender_comps.push(comps[i]);
@@ -82,7 +81,7 @@ YSLOW.registerRule({
   name: 'Load the 3rd party JS only once',
   info: 'Loading the 3rd party JS files more than once per page is not ' +
         'necessary and slows down the user experience',
-  category: ['Common'],
+  category: ['js'],
   config: {},
   url: 'http://www.phpied.com/3PO#once',
   
@@ -127,7 +126,99 @@ YSLOW.registerRule({
   }
 });
 
+YSLOW.registerRule({
+  id: 'cssprint',
+  name: 'Do not load print stylesheets, use @media',
+  info: 'Loading a specific stylesheet for printing slows down the page, ' +
+        'even though it is not used',
+  category: ['css'],
+  config: {},
+  url: 'http://sitespeed.io/rules/#cssprint',
+  
+  lint: function (doc, cset, config) {
+  var i, media, score,url,
+        offenders = [],
+        hash = {},
+        comps = cset.getComponentsByType('css'),
+        links = doc.getElementsByTagName('link');
 
+         for (i = 0, len = links.length; i < len; i += 1) {
+  
+          if (links[i].media === 'print') {
+            url = links[i].href;           
+            hash[url] = 1;
+          }
+        }
+
+     for (var i = 0; i < comps.length; i++) {
+      if (hash[comps[i].url]) {
+        offenders.push(comps[i]);
+      }
+    }
+      score = 100 - offenders.length * 20;
+      
+      return {
+      score: score,
+      message: (offenders.length > 0) ? YSLOW.util.plural(
+          'There %are% %num% print css files included on the page',
+          offenders.length
+      ) : '',
+      components: offenders
+    };
+
+
+   }
+});  
+
+YSLOW.registerRule({
+  id: 'cssinhead',
+  name: 'CSS in head ',
+  info: 'Make sure css in head is loaded from same domain as document',
+  category: ['css'],
+  config: {points: 5},
+  url: 'http://sitespeed.io/rules/#cssinhead',
+
+    lint: function (doc, cset, config) {
+        var i, len, score, domain, docDomain,
+            cssinhead = [],
+            domains = [],
+            offenders = [],
+            comps = cset.getComponentsByType('css');
+
+        for (i = 0, len = comps.length; i< len; i += 1) {
+    
+             if (comps[i].containerNode === 'head'){
+                cssinhead.push(comps[i]);
+            } 
+        }
+
+        docDomain = YSLOW.util.getHostname(cset.doc_comp.url);
+        domains = YSLOW.util.getUniqueDomains(cssinhead);
+
+         for (var i = 0, len = domains.length; i < len; i += 1) {
+            domain = domains[i];
+            if (domain !== docDomain) {
+                offenders.push(domain);
+            }
+        }
+
+        score = 100;
+        if (offenders.length >0) {
+              score -= 1 + offenders.length * parseInt(config.points, 5);
+        }
+
+        return {
+            score: score,
+            message: 'Docdomain:' + docDomain + ' domains:' + domains+ ' cssinhead:' + cssinhead + " comp:" + comps,
+            /*message: (offenders.length > 1) ? YSLOW.util.plural(
+                'There %are% %num% stylesheet%s%',
+                offenders.length
+            ) + ' loaded from a domain that is not the document domain' : '',
+            */
+            components: offenders
+        };
+    }
+});
 
 
 
@@ -144,6 +235,8 @@ YSLOW.registerRuleset({
         	// number of background images allowed before we start penalizing
         	max_cssimages: 3
         },
+        cssprint: {},
+        cssinhead: {},
         yemptysrc: {},
         yexpires: {},
         //  3
@@ -185,11 +278,14 @@ YSLOW.registerRuleset({
         // 31
         yfavicon: {},
         _3po_asyncjs: {},
-    	_3po_jsonce: {}
+	     _3po_jsonce: {}
+
 
     },
     weights: {
         ynumreq: 8,
+        cssprint: 1,
+        cssinhead: 8,
         yemptysrc: 30,
         yexpires: 10,
         ycompress: 8,
@@ -212,7 +308,7 @@ YSLOW.registerRuleset({
         yimgnoscale: 3,
         yfavicon: 2,
         _3po_asyncjs: 10,
- 	   	_3po_jsonce: 10	   	
+		    _3po_jsonce: 10		
     }
 
 });
