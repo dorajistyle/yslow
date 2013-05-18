d29e10fbc2c46888712aa68cbb5b95c1476ef866
Peter Hedenskog <peter@soulgalore.com>
2013-05-18 23:28:11 +0200

    referencing to the right rfc

--- src/common/component.js
+++ src/common/component.js
@@ -141,7 +141,8 @@ YSLOW.Component.prototype.populateProperties = function (resolveRedirect, ignore
     that.uncompressed_size = that.body.length;
 
     // expiration based on either Expires or Cache-Control headers
-    // http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.21
+    // http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.9.3
+ 
     if (that.getMaxAge() !== UNDEF) {
 	that.expires = that.getMaxAge();
     }
