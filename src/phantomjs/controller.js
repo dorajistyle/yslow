0153e6036e480bf002f1e860867e7591be834154
Peter Hedenskog <peter@soulgalore.com>
2013-08-04 21:37:22 +0200

    added comments

--- src/phantomjs/controller.js
+++ src/phantomjs/controller.js
@@ -15,7 +15,8 @@
 // For using yslow in phantomjs, see instructions @ https://github.com/marcelduran/yslow/wiki/PhantomJS
 
 
-// FROM netsniff.js
+// FROM netsniff.js borrowed from PhantomJS
+// https://github.com/ariya/phantomjs/blob/master/examples/netsniff.js
 if (!Date.prototype.toISOString) {
     Date.prototype.toISOString = function () {
         function pad(n) { return n < 10 ? '0' + n : n; }
